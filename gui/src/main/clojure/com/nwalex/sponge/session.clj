9ce8bf9195016dee9983528d2b480533b2796f01
nwalex <nwalex@83f18ab7-c4b7-4817-8de3-1920f1983931>
2010-03-08 21:33:25 +0000

    [#48] Added file chooser to the session

--- gui/src/main/clojure/com/nwalex/sponge/session.clj
+++ gui/src/main/clojure/com/nwalex/sponge/session.clj
@@ -1,6 +1,15 @@
 (ns com.nwalex.sponge.session
   (:require
+   [com.nwalex.sponge.gui-state :as state]
+   [com.nwalex.sponge.table-model :as model]
    [clojure.contrib.logging :as log]))
+
+(defn- choose-file []
+  (let [file-chooser (doto (javax.swing.JFileChooser.)
+                       (.setApproveButtonText "Save"))
+        response (.showOpenDialog file-chooser (state/gui))]
+    (if (= response javax.swing.JFileChooser/APPROVE_OPTION)
+      (.getSelectedFile file-chooser))))
 
 (defn load-session [event]
   (log/info "Ready to load session"))
@@ -9,4 +18,8 @@
   (log/info "Ready to save session"))
 
 (defn save-session-as [event]
-  (log/info "Ready to save session as"))
+  (let [file (choose-file)]
+    (if file
+      (do
+        (log/info (format "Ready to save in file %s" file)))
+      (log/info "No file chosen"))))
