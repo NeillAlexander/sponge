f82af37d3f393874cdacad5fb6f86b472567b64f
nwalex <nwalex@83f18ab7-c4b7-4817-8de3-1920f1983931>
2010-03-21 12:39:05 +0000

    [#86] Added license pre-amble to all source files

--- gui/src/main/java/com/nwalex/sponge/gui/ConfigurationDialog.java
+++ gui/src/main/java/com/nwalex/sponge/gui/ConfigurationDialog.java
@@ -1,13 +1,12 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/*
- * ConfigurationDialog.java
- *
- * Created on 04-Mar-2010, 20:23:04
- */
+/**
+* Copyright (c) Neill Alexander. All rights reserved.
+* The use and distribution terms for this software are covered by the
+* Eclipse Public License 1.0 (http://opensource.org/licenses/eclipse-1.0.php)
+* which can be found in the file epl-v10.html at the root of this distribution.
+* By using this software in any fashion, you are agreeing to be bound by
+* the terms of this license.
+* You must not remove this notice, or any other, from this software
+*/
 
 package com.nwalex.sponge.gui;
 
