42973573a1b10012b572e77fcf48a924f33c9b3e
nwalex <nwalex@83f18ab7-c4b7-4817-8de3-1920f1983931>
2010-04-01 12:54:20 +0000

    Fixed bat file. Now has memory settings and SPONGE_SESSIONS set

--- gui/src/main/bin/sponge.bat
+++ gui/src/main/bin/sponge.bat
@@ -2,8 +2,12 @@
 
 REM Build the classpath using SPONGE_HOME as the reference point
 if "%SPONGE_HOME%" == "" goto error
+
 set CLASSPATH=%SPONGE_HOME%\lib\*
-java com.nwalex.sponge.Client
+
+set SPONGE_SESSIONS="%SPONGE_HOME%\sessions"
+
+java -Xms128m -Xmx256m -Dsponge.sessions="%SPONGE_SESSIONS%"  com.nwalex.sponge.Client
 
 goto :end
 
