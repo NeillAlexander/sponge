f52983926aeea87dbae70d785d414e7e434e6a03
nwalex <nwalex@83f18ab7-c4b7-4817-8de3-1920f1983931>
2010-04-14 16:03:46 +0000

    [#26] Started working on the discovery code for the plugins

--- gui/src/main/java/com/nwalex/sponge/gui/plugins/PluginManager.java
+++ gui/src/main/java/com/nwalex/sponge/gui/plugins/PluginManager.java
@@ -10,6 +10,12 @@
 package com.nwalex.sponge.gui.plugins;
 
 import java.awt.event.ActionEvent;
+import java.io.File;
+import java.io.IOException;
+import java.util.Enumeration;
+import java.util.jar.JarEntry;
+import java.util.jar.JarFile;
+import java.util.logging.Level;
 import javax.swing.AbstractAction;
 import javax.swing.Action;
 import org.apache.log4j.Logger;
@@ -28,7 +34,17 @@ public class PluginManager {
   }
 
   private void findAllPlugins() {
-    System.out.println("ready to find all plugins");
+    File pluginDir = new File(System.getProperty("sponge.home"), "plugins");
+    System.out.println("ready to find all plugins in: " + pluginDir.getAbsolutePath());
+
+    if (!pluginDir.exists()) {
+      throw new RuntimeException("Plugin directory does not exist: " + pluginDir.getAbsolutePath());
+    }
+
+    File[] jarFiles = pluginDir.listFiles();
+    for (int i = 0; i < jarFiles.length; i++) {
+      findPluginsInJarFile(jarFiles[i]);
+    }
   }
 
   public Action getFindPluginsAction() {
@@ -38,5 +54,32 @@ public class PluginManager {
         findAllPlugins();
       }
     };
+  }
+
+  private void findPluginsInJarFile(File file) {
+    
+    try {
+      System.out.println("Checking for plugins in: " + file.getAbsolutePath());
+      JarFile jarFile = new JarFile(file);
+
+      Enumeration<JarEntry> entries = jarFile.entries();
+      
+      while (entries.hasMoreElements()) {
+        JarEntry entry = entries.nextElement();
+        System.out.println("Checking entry: " + entry.getName());
+      }
+
+      // TODO: create a classloader for every plugin jar
+      // TODO: use the classloader to try to load every class
+      // TODO: once class loaded check that it's non-abstract, and implements Plugin
+      // TODO: if so add to the list of available plugins
+      // TODO: also register the name of the plugin with the classloader
+      // TODO: give it a unique internal id to reference it by?
+      // TODO: then when enabled use the classloader to load it (or just load once found)
+      // TODO: would this be really slow for large jar files???
+
+    } catch (IOException ex) {
+      log.warn("Failed to load plugins from: " + file.getAbsolutePath(), ex);
+    }
   }
 }
