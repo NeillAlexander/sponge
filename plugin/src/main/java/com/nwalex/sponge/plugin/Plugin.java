e235a5d762688060d94c514af7fd322916baa510
nwalex <nwalex@83f18ab7-c4b7-4817-8de3-1920f1983931>
2010-04-14 12:19:50 +0000

    [#26] More work on the initial plugin implementation

---
+++ plugin/src/main/java/com/nwalex/sponge/plugin/Plugin.java
@@ -0,0 +1,46 @@
+/**
+* Copyright (c) Neill Alexander. All rights reserved.
+* The use and distribution terms for this software are covered by the
+* Eclipse Public License 1.0 (http://opensource.org/licenses/eclipse-1.0.php)
+* which can be found in the file epl-v10.html at the root of this distribution.
+* By using this software in any fashion, you are agreeing to be bound by
+* the terms of this license.
+* You must not remove this notice, or any other, from this software
+*/
+package com.nwalex.sponge.plugin;
+
+import org.apache.log4j.Logger;
+
+/**
+ * top level for all plugins
+ * @author alexanc
+ */
+public abstract class Plugin {
+
+  private static final Logger log = Logger.getLogger(Plugin.class);
+
+  // lifecycle methods
+  public abstract String getName();
+  public abstract LifecyclePoint getLifecyclePoint();
+  public abstract void enabled(PluginContext context);
+  public abstract void disabled(PluginContext context);
+
+  /**
+   * this is called by the application
+   * @param data
+   * @param context
+   * @return
+   */
+  public final Object execute(String data, PluginContext context) {
+    Object response = doPluginTask(data, context);
+    
+    if (!context.isValidResponse(response)) {
+      log.warn("Invalid response from plugin - returning unmodified data");
+      response = context.getResponseBuilder().buildContinueResponse(data);
+    }
+
+    return response;
+  }
+
+  public abstract Object doPluginTask(String data, PluginContext context);
+}
---
+++ plugin/src/main/java/com/nwalex/sponge/plugin/RequestPlugin.java
@@ -0,0 +1,35 @@
+/**
+* Copyright (c) Neill Alexander. All rights reserved.
+* The use and distribution terms for this software are covered by the
+* Eclipse Public License 1.0 (http://opensource.org/licenses/eclipse-1.0.php)
+* which can be found in the file epl-v10.html at the root of this distribution.
+* By using this software in any fashion, you are agreeing to be bound by
+* the terms of this license.
+* You must not remove this notice, or any other, from this software
+*/
+package com.nwalex.sponge.plugin;
+
+/**
+ * Called before the request is sent
+ * @author alexanc
+ */
+public abstract class RequestPlugin extends Plugin {
+
+  @Override
+  public final LifecyclePoint getLifecyclePoint() {
+    return LifecyclePoint.BEFORE_SEND_REQUEST;
+  }
+
+  @Override
+  public Object doPluginTask(String data, PluginContext context) {
+    return preProcessRequest(data, context);
+  }
+
+  /**
+   * This will be called just before the soap request is sent
+   * @param soapRequest
+   * @param context
+   * @return
+   */
+  public abstract Object preProcessRequest(String soapRequest, PluginContext context);
+}
