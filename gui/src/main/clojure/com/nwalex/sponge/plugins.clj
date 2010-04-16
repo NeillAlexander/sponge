d9dded04b7e541ebd761c338428162b006fb19d1
nwalex <nwalex@83f18ab7-c4b7-4817-8de3-1920f1983931>
2010-04-16 12:12:24 +0000

    [#26] Added code to register plugins

--- gui/src/main/clojure/com/nwalex/sponge/plugins.clj
+++ gui/src/main/clojure/com/nwalex/sponge/plugins.clj
@@ -2,11 +2,40 @@
 
 (def plugin-manager (ref nil))
 
+(def request-plugins (ref {}))
+(def response-plugins (ref {}))
+
+(def lifecycle-mapping
+     {com.nwalex.sponge.plugin.LifecyclePoint/BEFORE_REQUEST :request
+      com.nwalex.sponge.plugin.LifecyclePoint/AFTER_RESPONSE :response})
+
+(def lifecycle-store-mapping
+     {:request request-plugins
+      :response response-plugins})
+
+(defn- register-plugin [phase plugin]
+  (println (format "Register plugin %s for phasef %s" plugin phase))
+  (let [plugin-store (lifecycle-store-mapping phase)]
+    (dosync
+     (ref-set plugin-store (assoc @plugin-store (hash plugin) plugin)))
+    (println @plugin-store)))
+
+(defn- deregister-plugin [phase plugin]
+  (println (format "De-register plugin %s for phasef %s" plugin phase))
+  (let [plugin-store (lifecycle-store-mapping phase)]
+    (dosync
+     (ref-set plugin-store (dissoc @plugin-store (hash plugin))))
+    (println @plugin-store)))
+
 (defn enable-plugin [plugin]
-  (println (format "Ready to enable plugin %s" plugin)))
+  (println (format "Ready to enable plugin %s" plugin))
+  (let [lifecycle-point (lifecycle-mapping (.getLifecyclePoint plugin))]
+    (register-plugin lifecycle-point plugin)))
 
 (defn disable-plugin [plugin]
-  (println (format "Ready to disable plugin %s" plugin)))
+  (println (format "Ready to disable plugin %s" plugin))
+  (let [lifecycle-point (lifecycle-mapping (.getLifecyclePoint plugin))]
+    (deregister-plugin lifecycle-point plugin)))
 
 (def plugin-controller
      (proxy [com.nwalex.sponge.gui.plugins.PluginController] []
